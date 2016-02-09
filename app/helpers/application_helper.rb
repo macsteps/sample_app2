3ae3ea679f507d86fd142b4aeb154cc3bdfc6f02
macsteps <macsteps@gmail.com>
2016-02-09 08:34:51 -0800

    add full_title helper and rm title from home.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Returns the title on a full-page basis.
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
