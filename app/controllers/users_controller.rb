792556d1d199eb1a7bf99e5eb0d34717eb98275f
macsteps <macsteps@gmail.com>
2016-02-15 08:34:46 -0800

    add flash msg on successful signup and configure all pages to display alert messages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -24,5 +25,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-    
+
 end
