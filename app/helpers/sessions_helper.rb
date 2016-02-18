3c0e260556b62179e06f81464c5e663a8a8b09ec
macsteps <macsteps@gmail.com>
2016-02-18 13:36:56 -0800

    add current_user method to determine the user based on the session user id.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,13 @@
 module SessionsHelper
 
+  # Log in the given user.
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  # Create and return currently logged in user (if any) as @current_user.
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
   end
   
 end
