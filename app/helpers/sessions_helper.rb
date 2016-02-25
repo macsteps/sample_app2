c68129de6ecca212b77e7ae839f371023dfcc43e
macsteps <macsteps@gmail.com>
2016-02-24 17:08:30 -0800

    ensure user can only edit, update their own profile/settings, and tests.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,11 @@ module SessionsHelper
     cookies.permanent.signed[:remember_token] = user.remember_token
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   # Create and return currently logged in user (if any) as @current_user.
   def current_user
     if (user_id = session[:user_id])
