d24ca2257267a22cfcd16a078cb8b96961d9da25
macsteps <macsteps@gmail.com>
2016-02-28 11:04:32 -0800

    add activations controller, migration, and seed/fixtures.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root                'static_pages#home'
   get 'help'      =>  'static_pages#help'
   get 'about'     =>  'static_pages#about'
@@ -10,5 +8,6 @@ Rails.application.routes.draw do
   post 'login'    =>  'sessions#create'
   delete 'logout'  =>  'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 
 end
