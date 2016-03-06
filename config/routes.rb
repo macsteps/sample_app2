989dfb08cafdf7240475ebf431f22e116237ed40
macsteps <macsteps@gmail.com>
2016-03-06 12:36:43 -0800

    display followers/following, add seed data, and test.

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,14 @@ Rails.application.routes.draw do
   get 'signup'    =>  'users#new'
   get 'login'     =>  'sessions#new'
   post 'login'    =>  'sessions#create'
-  delete 'logout'  =>  'sessions#destroy'
-  resources :users
-  resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :microposts, only: [:create, :destroy]
+  delete 'logout' =>  'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :account_activations,   only: [:edit]
+  resources :password_resets,       only: [:new, :create, :edit, :update]
+  resources :microposts,            only: [:create, :destroy]
+  resources :relationships,         only: [:create, :destroy]
 end
